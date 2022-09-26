56f69b8340a3f759af8aa5a59c1c3cff4e48900f
huynang23 <huynang09@gmail.com>
2022-09-26 16:12:18 +0700

    btdemchu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import debounce from 'lodash.debounce'
 
 function App() {
+  // the text entered by the user
+  const [text, setText] = useState('');
+
+  // word count
+  const [wordCount, setWordCount] = useState(0);
+
+  // character count
+  // const [charCount, setCharCount] = useState(0);
+
+  //500ms
+
+  const changeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    // array of words
+    const words = text.split(' ');
+
+    // update word count
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+
+    // update char count 
+    // setCharCount(text.length);
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>
+        <input type={text}
+          value={text}
+          onChange={changeHandler}
+          placeholder='Enter text...'
+        ></input>
+
+        <div>
+          <p className='word-count'>Số chữ: {wordCount}</p>
+          {/* <p className='char-count'>
+            số kí tự: {charCount}
+          </p> */}
+        </div>
+      </div>
     </div>
   );
 }
